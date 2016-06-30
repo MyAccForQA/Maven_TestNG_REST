131409d0bdf4054573ec05385349d51695aacac0
PFName PLNmae <PFNamePLNmae@gmail.com>
2016-06-30 00:34:09 -0700

    Maven_TestNG_REST

---
+++ src/main/java/servlets/LoginFilter.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    private FilterConfig filterConfig = null;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (filterConfig == null)
+            return;
+
+        String user = (String) filterConfig.getServletContext().getAttribute("login");
+        if(user != null){
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        servletRequest.getRequestDispatcher("/login").forward(servletRequest, servletResponse);
+
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+}
